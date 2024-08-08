8f429d5fd8af205d8c5ba152be57bd0fcfcb687e
R.Can Pahalı <canpahali@gmail.com>
2024-08-08 15:53:09 +0200

    tower take dmg wip

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -2,10 +2,19 @@ extends Sprite2D
 
 @onready var damage_timer: Timer = $DamageTimer
 @onready var tower_fire: Sprite2D = $TowerFire
+# set progress bar
+
 
 var enemy_list: Array[CharacterBody2D] = []
 
-const HEALTH = 10000
+const SELF_DAMAGE = 200.0
+var HEALTH = 10000.0
+
+var attacking_enemies_list: Array[CharacterBody2D] = []
+
+func _process(_delta: float) -> void:
+	if attacking_enemies_list.size() > 0:
+		take_self_damage()
 
 func _on_tower_area_body_entered(body: CharacterBody2D) -> void:
 	if body.is_in_group("enemy"):
@@ -34,3 +43,18 @@ func run_fire_animation():
 	tower_fire.visible = true
 	await get_tree().create_timer(0.3).timeout
 	tower_fire.visible = false
+
+########## TAKE DAMAGE ##########
+
+func _on_hurt_box_body_entered(body:CharacterBody2D) -> void:
+	attacking_enemies_list.push_back(body)
+
+func _on_hurt_box_body_exited(body:Node2D) -> void:
+	var index = attacking_enemies_list.find(body)
+	if index != -1:
+		attacking_enemies_list.remove_at(index)
+
+func take_self_damage():			
+	HEALTH = HEALTH - (SELF_DAMAGE * attacking_enemies_list.size())
+	if HEALTH < 0:
+		self.modulate = Color.RED
